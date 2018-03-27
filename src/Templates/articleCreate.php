a2fc42e33ed190c3007bd480f4ab707b92222b27
LallemandE <job@lallemand.lu>
2018-03-27 16:39:41 +0200

    articleCreate Template done !

---
+++ src/Templates/articleCreate.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Article Creation</title>
+  </head>
+  <body>
+    <form method="POST">
+      <input type="text" name="title" value="" placeholder="Article Name ? ...">
+      <input type="text" name="url" value="" placeholder="image URL ? ...">
+      <textarea name="description" rows="4" cols="50">Article Description ? ....</textarea>
+    </form>
+  </body>
+</html>
