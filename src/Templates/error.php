346eed9ff678091833b3bd6c4d06c67444d27255
LallemandE <job@lallemand.lu>
2018-03-28 13:36:58 +0200

    error.php template => real html page

--- src/Templates/error.php
+++ src/Templates/error.php
@@ -1,4 +1,16 @@
-<?php
-echo '<h1>Error</h1>';
-echo '<h2>' . ($controller->errorCode ?? 500) . '</h2>';
-echo '<p>' . ($controller->errorMessage ?? "Internal Error") . '</p>';
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Error Page</title>
+    <link rel="stylesheet" href="/css/main.css">
+  </head>
+  <body>
+    <h1>ARTICLE DELETED</h1>
+    <?php
+    echo '<h1>Error</h1>';
+    echo '<h2>' . ($errorCode ?? 500) . '</h2>';
+    echo '<p>' . ($errorMessage ?? "Internal Error") . '</p>';
+    ?>
+  </body>
+</html>
